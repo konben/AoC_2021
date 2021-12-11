e0894102085a441a64754ace078f7c4ff4194dd3
Benedict Konhäuser <bene@debian-BULLSEYE-live-builder-AMD64>
2021-12-11 21:01:33 +0100

    Adding a solution to part two

--- day_2/dive.py
+++ day_2/dive.py
@@ -2,8 +2,6 @@
 
 # Part one
 
-course = open('course')
-
 def moveSub(course) -> tuple:
     """
     Computes the position of the submarine after following a course.
@@ -20,12 +18,13 @@ def moveSub(course) -> tuple:
     # Reading directions
     for line in course:
         direction, steps = line.split()
+        steps = int(steps)
         if direction == 'forward':
-            distance += int(steps)
+            distance += steps
         elif direction == 'up':
-            depth -= int(steps)
+            depth -= steps
         elif direction == 'down':
-            depth += int(steps)
+            depth += steps
         else:
             # Bad input!
             raise(Exception('This shouldnt have happened!'))
@@ -33,6 +32,47 @@ def moveSub(course) -> tuple:
     return (distance, depth)
 
 # Main
+course = open('course')
 dist, depth = moveSub(course)
+print('Part one:')
+print(f"The submarine moved {dist} steps forward and {depth} steps down!")
+print(f"The product of the dept and distance traveled is {dist*depth}!")
+
+# Part two
+
+def moveSub2(course) -> tuple:
+    """
+    Computes the position of the submarine after following a course.
+
+    Arguments:
+        course -- File containing directions of the form <direction> <steps>.
+    Returns:
+        The position after follonwing the course as a tuple (distance, depth).
+    """
+
+    # Initial position
+    distance, depth, aim = 0, 0, 0
+
+    # Reading directions
+    for line in course:
+        direction, steps = line.split()
+        steps = int(steps)
+        if direction == 'forward':
+            distance += steps
+            depth += steps*aim
+        elif direction == 'up':
+            aim -= steps
+        elif direction == 'down':
+            aim += steps
+        else:
+            # Bad input!
+            raise(Exception('This shouldnt have happened!'))
+
+    return (distance, depth)
+
+# Main
+course = open('course')
+dist, depth = moveSub2(course)
+print('Part two:')
 print(f"The submarine moved {dist} steps forward and {depth} steps down!")
 print(f"The product of the dept and distance traveled is {dist*depth}!")
